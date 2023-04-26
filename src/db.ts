37ee1a99c737c3f64233cb5de6f69090f30212d3
Manuel Mena <manuelmena1993@gmail.com>
2023-04-26 06:57:36 -0300

    chore: insecure

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,9 @@
 import { Connection, connect } from '@planetscale/database'
 
 const config = {
-  host: '',
-  username: '',
-  password: ''
-  
+  host: 'aws.connect.psdb.cloud',
+  username: 'my8rosgp4sj0w579mbxy',
+  password: 'pscale_pw_sLIvnvEaktswRFx4F4fZaIeRX7qbGQl2BHqd0lexF8b'
 }
 
 const conn: Connection = connect(config)
