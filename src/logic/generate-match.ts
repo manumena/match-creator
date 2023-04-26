a60e803320c7ecb0991b0a582ca1bf3f6bf503f4
Manuel Mena <manuelmena1993@gmail.com>
2023-04-26 06:30:52 -0300

    feat: generate-match returns array of anime data

--- src/logic/generate-match.ts
+++ src/logic/generate-match.ts
@@ -2,12 +2,30 @@ import { getRandomNumbers } from "./random";
 import { conn } from "../db";
 
 const QUERY_GET_TOTAL_AMOUNT_OF_SONGS = 'SELECT COUNT(*) FROM songs;';
+const QUERY_GET_SONGS_BY_ID = 'SELECT * FROM songs WHERE {};';
 
 export async function generateMatch() {
   // Get total amount of songs
-  const queryResponse = await conn.execute(QUERY_GET_TOTAL_AMOUNT_OF_SONGS) as any
+  let queryResponse = await conn.execute(QUERY_GET_TOTAL_AMOUNT_OF_SONGS) as any
   const totalSongs = parseInt(queryResponse.rows[0]['count(*)'])
 
-  const randomNumbers = getRandomNumbers(3, totalSongs)
-  return randomNumbers
+  // Get random ids
+  const randomIds = getRandomNumbers(3, totalSongs)
+
+  // Use random ids to get the songs
+  const selectedSongsResponse = await conn.execute(buildSelectedIdsQuery(randomIds))
+  return selectedSongsResponse.rows
+}
+
+function buildSelectedIdsQuery(ids: number[]) {
+  // Set first id
+  let condition = `id = ${ids[0]}`
+
+  // Set the rest of them
+  for (let pos = 1; pos < ids.length; pos++) {
+    condition = condition + ` OR id = ${ids[pos]}`
+  }
+
+  // Replace the condition in the query template
+  return QUERY_GET_SONGS_BY_ID.replace('{}', condition)
 }
