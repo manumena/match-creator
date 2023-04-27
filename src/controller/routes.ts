6a9ba1df5329e53e0a2bef45c61aff07a1c6b152
Manuel Mena <manuelmena1993@gmail.com>
2023-04-27 17:46:32 -0300

    feat: add cors

--- src/controller/routes.ts
+++ src/controller/routes.ts
@@ -4,6 +4,12 @@ import generateMatchHandler from './handlers/generate-match-handler'
 
 const router = new Router()
 
+router.cors({
+  allowOrigin: '*',
+  allowMethods: '*',
+  allowHeaders: '*'
+})
+
 router.get('/', hiHandler)
 router.get('/hi', hiHandler)
 router.get('/generate-match', generateMatchHandler)
