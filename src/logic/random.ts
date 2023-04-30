ff90878711c4eaa624dbb3c9ead9916a48fcceb6
Manuel Mena <manuelmena1993@gmail.com>
2023-04-30 01:08:46 -0300

    feat: add randomElementsFromList

--- src/logic/random.ts
+++ src/logic/random.ts
@@ -1,14 +1,19 @@
-export function getRandomNumbers(quantity: number, maximumValue: number) {
+export function getRandomNumbers(quantity: number, min: number, max: number): number[] {
   const result: number[] = []
   for (let counter = 0; counter < quantity; counter++) {
     // Generate a candidate until is not already included in the results
     let candidate
     do {
-      candidate = Math.floor(Math.random() * maximumValue) + 1
+      candidate = Math.floor(Math.random() * max) + min
     } while (result.includes(candidate))
     
     // Add candidate to results
     result.push(candidate)
   }
   return result
+}
+
+export function getRandomElementsFromList(list: any[], amount: number): any[] {
+  const randomPositions = getRandomNumbers(amount, 0, list.length - 1)
+  return randomPositions.map(pos => list[pos])
 }
